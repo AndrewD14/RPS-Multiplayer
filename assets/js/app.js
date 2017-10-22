7a41f2ebb90890cef3c65ea33141dc15a2481969
AndrewD14 <Leingod2005@gmail.com>
2017-10-22 16:58:26 -0500

    Modify the user child_added function to update the current player's name. Added in html code to display the player's info. Added in the function to populate the current player's choices.

--- assets/js/app.js
+++ assets/js/app.js
@@ -19,15 +19,23 @@ var userName = "";
 var disconnetRef = "";
 var activePlayerNum = 0;
 
-//updates the player count
+//event trigger for when new child is added to users
 database.ref("/users").on("child_added", function(snapshot){
+	//gets the players count
 	database.ref().once("value").then(function(snapshot2){
 		playerCount = 0; //resets since this is called for all childs when a page loads
 		for(i in snapshot2.val().users){
 			playerCount++;
 			database.ref("/users/"+i).once("value").then(function(snapshot3){
 				activePlayerNum = snapshot3.val().player;
-				console.log(snapshot3.val().player)
+
+				//displays the player's name if others are active
+				if(activePlayerNum == 1){
+					$("#playerName1").html(snapshot3.val().userName);
+				}
+				else if(activePlayerNum == 2){
+					$("#playerName2").html(snapshot3.val().userName);
+				}
 			});
 		}
 	});
@@ -38,19 +46,24 @@ function addUser(name){
 	var users = database.ref("/users");
 	var key = "";
 
-	//adds player 1
+	//checks to see if there are 2 players
 	if(playerCount == 0 || playerCount == 1){
+		//adds player 1
 		if(activePlayerNum == 2 || activePlayerNum == 0){
 			key = users.push({
 				userName: name,
-				player: 1
+				player: 1,
+				wins: 0,
+				losses: 0
 			});
 		}
 		//adds player 2
 		else if(activePlayerNum == 1){
 			key = users.push({
 				userName: name,
-				player: 2
+				player: 2,
+				wins: 0,
+				losses: 0
 			});
 		}
 	}
@@ -59,8 +72,10 @@ function addUser(name){
 	player = key.key;
 
 	//disconnect event
-	if(player != "")
+	if(player != ""){
 		disconnetRef = database.ref("/users/"+player).onDisconnect().remove();
+		displayCurrentPlayer();
+	}
 }
 
 //click function for joining the game
@@ -73,4 +88,55 @@ function logIn(){
 
 	//removes the log in form
 	$("#join").remove();
+}
+
+//display player
+function displayCurrentPlayer(){
+	var playerNum;
+	var playerName;
+	database.ref("/users/"+player).once("value").then(function(snapshot2){
+		playerName = snapshot2.val().userName;
+		$("#name").html(playerName);
+		
+		playerNum = snapshot2.val().player;
+		$("#playerNum").html(playerNum);
+
+		var rock = $("<p>");
+		rock.html("Rock");
+		rock.attr("data", "r");
+		rock.addClass("choice");
+
+		var paper = $("<p>");
+		paper.html("Paper");
+		paper.attr("data", "p");
+		paper.addClass("choice");
+
+		var scissor = $("<p>");
+		scissor.html("Scissor");
+		scissor.attr("data", "s");
+		scissor.addClass("choice");
+
+		var wins = $("<p>");
+		wins.html("<b>Wins: </b>"+snapshot2.val().wins);
+
+		var losses = $("<p>");
+		losses.html("<b>Losses: </b>"+snapshot2.val().losses);
+
+		if(playerNum == 1){
+			$("#playerName1").html(playerName);
+			$("#player1").append(rock);
+			$("#player1").append(paper);
+			$("#player1").append(scissor);
+			$("#player1").append(wins);
+			$("#player1").append(losses);
+		}
+		else if(playerNum == 2){
+			$("#playerName2").html(playerName);
+			$("#player2").append(rock);
+			$("#player2").append(paper);
+			$("#player2").append(scissor);
+			$("#player2").append(wins);
+			$("#player2").append(losses);
+		}
+	});
 }
